7567841eeb55c843f9cbe6c368cc67819648d93d
FelipeLiberato005 <felipeliberato2005@gmail.com>
2024-11-14 00:34:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php
 	
+	//Definindo data e horario da região
 	date_default_timezone_set('America/Sao_Paulo');
+	//criando variavel que vai armazenar a conexão com o banco de dado inciando_a
 	$pdo = new PDO('mysql:host=localhost;dbname=inciaindo_a','root','root');
 
+	////Testando conexão
 	if(isset($_POST['acao'])){
+	//Pegando valores das variaveis do formulario a baixo
 	$nome = $_POST['nome'];
 	$sobrenome = $_POST['sobrenome'];
+	//Usando função do php para pegar data e horario
 	$momento_registro = date('Y-m-d H:i:s');
 
-	$sql = $pdo->prepare("INSERT INTO `clientes` VALUES (null,?,?,?)");
 
+	$sql = $pdo->prepare("INSERT INTO `clientes` VALUES (null,?,?,?)");
+	//inseriindo valores a partir dos dados do formulario
 	$sql->execute(array($nome,$sobrenome,$momento_registro));
+	
+	//Se tudo ocorrer bem, então vai dar essa mensagem
 	echo "Inserido com sucesso";
 }
 ?>
 
-
+//Formulario basico para pegar dados do Cliente
 <!DOCTYPE html>
   <head>
     <title>Lista Clientes</title>
